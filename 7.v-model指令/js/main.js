7e602e06134f44bd8fdc951321e5edb7269252a7
ShuoyuGong <2788696894@qq.com>
2020-01-11 22:17:21 +0800

    once

---
+++ 7.v-model指令/js/main.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+        name:"龚烁宇",
+        age:20,
+    }
+});
+
+var app = new Vue({
+    el:"#app1",
+    data:{
+        name:"龚烁宇",
+        age:20,
+    }
+});
+
+var app = new Vue({
+    el:"#app2",
+    data:{
+        name:"龚烁宇",
+        age:20,
+    }
+});
+
+var app = new Vue({
+    el:"#app3",
+    data:{
+        name:"龚烁宇",
+        age:20,
+    }
+});
---
+++ 8.v-model在其他元素及类型上的用法/js/main.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+        sex:"male",
+        hobby:[
+            "篮球","台球","排球"
+        ]
+    }
+});
+
+var app = new Vue({
+    el:"#app1",
+    data:{
+        from:"郑州",
+        where:[],
+    }
+});
+
+var app = new Vue({
+    el:"#app2",
+    data:{
+        article:"因为爱情。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。",
+    }
+});
